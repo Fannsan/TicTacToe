0b7985be12b438ce9c45d885595c8be848b9b2ae
Fannsan <112868096+Fannsan@users.noreply.github.com>
2023-09-18 18:51:48 +0200

    Creating a function for boardcheck
    -Creating a function to check if board has vertical, diagonal and horisontal wins.
    -Creating a check if player 1 or 2 has won
    -deleting images

--- TestTicTacToe/Controllers/SecondViewController.swift
+++ TestTicTacToe/Controllers/SecondViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class SecondViewController: UIViewController {
     
 
-    @IBOutlet weak var ImgCollection: UIImageView!
+
+    @IBOutlet var imgCollection: [UIImageView]!
+    
     
     
     @IBOutlet weak var lblCurrentPlayer: UILabel!
@@ -19,68 +21,95 @@ class SecondViewController: UIViewController {
     
     var currentPlayer = 1
     
-    var board = [UIImage]()
+    var board = [0,0,0,0,0,0,0,0,0]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
-         //imgA1.isHidden = true
-       
     }
     
+    //Create a function to check who is the winner
+    func checkWinner() -> Bool{
+     
+        //check horisontal wins
+        if board[0] != 0 && board[0] == board [1] && board[1] == board[2]{
+            return true
+        }
+        
+        if board[3] != 0 && board[3] == board [4] && board[4] == board[5]{
+            return true
+        }
+        
+        if board[6] != 0 && board[6] == board [7] && board[7] == board[8]{
+            return true
+        }
+        //check vertical wins
+        if board[0] != 0 && board[0] == board [3] && board[3] == board[6]{
+            return true
+        }
+        if board[1] != 0 && board[1] == board [4] && board[4] == board[7]{
+            return true
+        }
+        if board[2] != 0 && board[2] == board [5] && board[5] == board[8]{
+            return true
+        }
+        //Diagonall wins
+        if board[0] != 0 && board[0] == board [4] && board[4] == board[8]{
+            return true
+        }
+        if board[2] != 0 && board[2] == board [4] && board[4] == board[6]{
+            return true
+        }
+        
+        return false
+    }
     
 
     
     @IBAction func tapped(_ sender: UITapGestureRecognizer) {
        
+        //fetching the imageView
         guard let attachedImageView = sender.view as? UIImageView else {return}
        
-    
+        guard let tag = sender.view?.tag else {return}
     
         //Check if imageView is empty
-        if attachedImageView.image == UIImage(systemName: ""){
+        if attachedImageView.image == UIImage(systemName: "") && board[tag] == 0{
             
             if currentPlayer == 1{
                 attachedImageView.image = UIImage(systemName: "xmark")
-                currentPlayer = 2
-                
-            }else {
-                currentPlayer = 1
-                attachedImageView.image = UIImage(systemName: "circle")
-                
-            }
+                board[tag] = 1
             
-            lblCurrentPlayer.text = currentPlayer.formatted()
+                    }else {
+                        attachedImageView.image = UIImage(systemName: "circle")
+                        board[tag] = 2
+                        }
+            
+                print(board)
+            
+          
     
-//            if checBoard(){
-//                print("filled board")
-//            }
+            if checkWinner(){
+              
+                print("player \(currentPlayer) wins!")
+                
+                //make an alert to show the winner and to reset the game
+                
+
+                    }  else {
+                    currentPlayer = 3 - currentPlayer
+                    lblCurrentPlayer.text = currentPlayer.formatted()
+                    }
+
+            } else{
+            //board is full
+            print("board is full or spot is taken")
+           
         }
 
     }
     
-    
-    func checBoard() -> Bool{
-     var filledCount = 0
-        
-        for imageView in ImgCollection.subviews{
-            if let image = imageView as? UIImageView, image.image != UIImage(systemName: ""){
-                filledCount += 1
-            }
-                
-        }
-        if filledCount == 9 {
-            
-           
-            return true
-            
-        }
-        
-        return false
-    }
     
     
     
